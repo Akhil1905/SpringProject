b189f45ac3b0c4ffcbaa1e0ddc858a21a9093783
Akhil <akhil.sharma@yantriks.com>
2021-02-13 03:55:10 +0000

    Spring Project

--- src/main/java/com/library/controller/CustomerController.java
+++ src/main/java/com/library/controller/CustomerController.java
@@ -7,6 +7,7 @@ import com.library.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,17 +25,43 @@ public class CustomerController {
     @ResponseStatus(HttpStatus.CREATED)
     public String addCustomer(@RequestBody Customer customer) {
         customerRepository.save(customer);
-
-       return "Added new customer to repo!";
+        return "Added new customer to repo!";
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<Customer> getCustomers() {
-        return customerRepository.findAll();
-    }
-//
-//    @GetMapping("/find/{id}")
-//    public Customer findCustomerById(@PathVariable Integer id) {
-//        return customerRepository.findCustomerById(id);
+//    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+//    public Iterable<Customer> getCustomers() {
+//        return customerRepository.findAll();
 //    }
+
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Customer getCustomerById(@PathVariable Integer id) {
+
+        return customerRepository.findCustomerById(id);
+    }
+
+    @RequestMapping(value="/{id}",produces = MediaType.APPLICATION_JSON_VALUE,method=RequestMethod.PUT)
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer,@PathVariable Integer id){
+
+        Customer updateCustomer=   customerRepository.findCustomerById(id);
+
+        updateCustomer.setId(customer.getId());
+        updateCustomer.setFirstName(customer.getFirstName());
+
+        updateCustomer.setLastName(customer.getLastName());
+        customerRepository.save(updateCustomer);
+
+        return new ResponseEntity<Customer>(updateCustomer, HttpStatus.OK);
+
+          }
+
+    @RequestMapping(value="/{id}",produces = MediaType.APPLICATION_JSON_VALUE,method=RequestMethod.DELETE)
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable Integer id){
+
+        Customer deleteCustomer=   customerRepository.findCustomerById(id);
+
+        customerRepository.delete(deleteCustomer);
+
+        return new ResponseEntity<Customer>(deleteCustomer, HttpStatus.NO_CONTENT);
+
+    }
 }
--- src/main/java/com/library/customerinfo/Customer.java
+++ src/main/java/com/library/customerinfo/Customer.java
@@ -11,7 +11,6 @@ public class Customer {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String firstName;
